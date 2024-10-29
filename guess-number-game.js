e6537a1e8d533646a8af7f97c748a25b4a946f73
Aanchal-wmp <aanchalsaxena@wingmanpartners.com>
2024-10-29 17:37:35 +0530

    Implemented number guesssing game

---
+++ guess-number-game.js
@@ -0,0 +1,19 @@
+// Generating a random integer between 1 and 10
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+// Prompt the user to input their guess
+const userInput = prompt("Guess a number between 1 and 10:");
+const userGuess = parseInt(userInput);
+
+// check if the input is a valid number or not and alert the user
+if (isNaN(userGuess)) {
+    alert("Invalid input, please enter a number :) ");
+} 
+
+else {
+    if (userGuess === randomNumber) {
+        alert("Good Work!! Your Guess was aboslutely correct!"); // Display this message if matched
+    } else {
+        alert(`Opps :(  Not matched. The correct number was ${randomNumber}.`); // Display tfis message if not matched
+    }
+}
