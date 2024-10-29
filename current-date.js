554e36f0023ecdd6f961ab895092d72c26ffae57
Aanchal-wmp <aanchalsaxena@wingmanpartners.com>
2024-10-29 17:38:38 +0530

    wrote current date program

--- current-date.js
+++ current-date.js
@@ -1,6 +1,12 @@
+//function to find the current date
 function getCurrentDate() {
     const date = new Date();
-    const day = date.getDate;
-    const month = date.getMonth;
-    const year = date.getFullYear;
+    const day = date.getDate();
+    const month = date.getMonth() +1;
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`;
 }
+
+//calling the fucntion getCurrentdate and console logging it
+
+console.log("Current Date in 'format day-month-year': ", getCurrentDate());
